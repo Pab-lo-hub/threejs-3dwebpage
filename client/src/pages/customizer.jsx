2db5628d3005d55d9f3fdca5925772b592df5c6f
Pab-lo-hub <salaberry.pablo@gmail.com>
2023-04-05 20:08:30 -0300

    deploy

--- client/src/pages/customizer.jsx
+++ client/src/pages/customizer.jsx
@@ -55,9 +55,9 @@ const Customizer = () => {
 
       const response = await fetch('https://devswag.onrender.com/api/v1/dalle', {
         method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
+        // headers: {
+        //   'Content-Type': 'application/json'
+        // },
         body: JSON.stringify({
           prompt,
         })
