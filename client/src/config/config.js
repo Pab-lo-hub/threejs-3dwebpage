afd5ca6e046df2c5ad3267dcbafb6a1813d5b094
Pab-lo-hub <salaberry.pablo@gmail.com>
2023-04-05 20:20:07 -0300

    deploy

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,9 +1,9 @@
 const config = {
   development: {
-    backendUrl: "https://devswag.onrender.com/api/v1/dalle",
+    backendUrl: "https://threejs-openai-fcop.onrender.com/api/v1/dalle",
   },
   production: {
-    backendUrl: "https://devswag.onrender.com/api/v1/dalle",
+    backendUrl: "https://threejs-openai-fcop.onrender.com/api/v1/dalle",
   },
 };
 
