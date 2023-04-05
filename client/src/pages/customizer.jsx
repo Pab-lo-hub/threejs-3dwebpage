958bab80486a04d6db2c610420146ca2550db531
Pab-lo-hub <salaberry.pablo@gmail.com>
2023-04-05 20:11:49 -0300

    deploy

--- client/src/pages/customizer.jsx
+++ client/src/pages/customizer.jsx
@@ -53,11 +53,11 @@ const Customizer = () => {
     try {
       setGeneratingImg(true);
 
-      const response = await fetch('https://devswag.onrender.com/api/v1/dalle', {
+      const response = await fetch('http://localhost:8080/api/v1/dalle', {
         method: 'POST',
-        // headers: {
-        //   'Content-Type': 'application/json'
-        // },
+        headers: {
+          'Content-Type': 'application/json'
+        },
         body: JSON.stringify({
           prompt,
         })
